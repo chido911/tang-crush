1a3ea9dfc25f92aa844149d9decbdb66c3ee51f3
chido911 <86825358+chido911@users.noreply.github.com>
2021-07-02 15:12:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Uyênnnnnnnnn à!',
+    introDesc: `Từ lâu rồi, tớ đã thích cậu á
+    Mà sao cậu cứ ở đâu í, nhưng mà không sao
+    Bây giờ tớ là của cậu rồi, hí hí
+    Cậu có muốn làm gì tớ hônggg`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnYes: 'Do you want to biuld the snowman?',
+    btnNo: 'Wth, không nha!',
+    question:'Đi đâu có tuyết nhỉ?',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/1343664728995085' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
